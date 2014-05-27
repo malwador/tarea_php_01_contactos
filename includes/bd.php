79d2307d21cbe2e30c67c4acb157e7e94be8a47c
Salvador Aguilar S <sal.aguilar81@gmail.com>
2014-05-27 16:10:00 -0600

    Agregando conexion a base de datos .. revise mi teacher!

--- includes/bd.php
+++ includes/bd.php
@@ -1,7 +1,20 @@
 <?php
 /**
- * Created by PhpStorm.
  * User: salvador
  * Date: 5/27/14
  * Time: 3:47 PM
- */ 
+ * Aqui vamos a listar la información de la base de datos
+ */
+
+// datos generales de conexion
+$server_bd = localhost;
+$user_bd = tarea_php;
+$pass_bd = 123456;
+$schema_db = tarea01;
+
+
+// vamos a intentar conectarnos
+$connect_db = new mysqli($server_bd,$user_bd,$pass_bd,$schema_db);
+    if ($connect_db->connect_errno) {
+        echo "Mierda! Algo salió mal con la base de datos: (" . $connect_db->connect_errno . ") " . $connect_db->connect_error;
+    }
