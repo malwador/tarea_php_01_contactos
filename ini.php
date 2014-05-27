72b8f2b979a6e16ddc8544f3eeb028ea956b276a
Salvador Aguilar S <sal.aguilar81@gmail.com>
2014-05-27 17:43:55 -0600

    completando formulario

--- ini.php
+++ ini.php
@@ -21,9 +21,34 @@ echo '<p><h1>Tarea PHP 01 - Gestión de contactos</h1></p>';
             <label>Nombres
                 <input type="text" placeholder="Escriba sólo sus nombres" />
             </label>
-            <label>
+            <label>Apellidos
                 <input type="text" placeholder="Escriba sus apellidos" />
             </label>
+            <label>Genero
+                <select>
+                    <option value="Masculino">Masculino</option>
+                    <option value="Femenino">Femenino</option>
+                </select>
+            </label>
+            <label>Departamento
+                <select>
+                    <option value="Managua">Managua</option>
+                    <option value="Masaya">Masaya</option>
+                    <option value="Granada">Granada</option>
+                    <option value="Rivas">Rivas</option>
+                    <option value="Leon">Leon</option>
+                    <option value="Chinandega">Chinandega</option>
+                    <option value="Boaco">Boaco</option>
+                    <option value="Chontales">Chontales</option>
+                    <option value="Rio San Juan">Rio San Juan</option>
+                    <option value="Jinotega">Jinotega</option>
+                    <option value="Nueva Segovia">Nueva Segovia</option>
+                    <option value="Matagalpa">Matagalpa</option>
+                    <option value="RAAN">RAAN</option>
+                    <option value="RAAS">RAAS</option>
+                </select>
+            </label>
+            <button type="submit" value="Agregar">
         </div>
     </div>
 
