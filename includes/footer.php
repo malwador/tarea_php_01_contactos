14dfc385ea5eb2da83d4bf8723719a9454e6a6f6
Salvador Aguilar S <sal.aguilar81@gmail.com>
2014-06-12 10:19:11 -0600

    componiendo footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,20 +1,12 @@
 
 <!--cerramos el div de inicio-->
 </div>
-<div class="row">
-<nav class="top-bar">
-    <section class="top-bar-section">
-        <ul class="right">
-            <li>1 One</li>
-            <li>2 Two</li>
-            <li>3 Three</li>
-        </ul>
-    </section>
+<div class="row marketing">
+<nav class="top-bar" data-topbar>
+        <div class="text-center" style="color: #ffffff">Código de <a href="mailto:salvador@iliux.com">Salvador Aguilar</a>. Revisalo en <a href="https://github.com/riper81/tarea_php_01_contactos" target="_blank"><i class="fa fa-github"></i> GitHub</a></div>
+</nav>
 </div>
 
-    <!--<div class="small-2 large-4 columns"><h4>Información General</h4></div>
-    <div class="small-4 large-4 columns"><h4>Donar</h4></div>
-    <div class="small-6 large-4 columns"><h4>Redes Sociales</h4></div>-->
 </nav>
 </body>
 </html>
